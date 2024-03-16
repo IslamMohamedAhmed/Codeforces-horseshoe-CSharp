e3025d074dbc43476ac9507ff7a5000211e0ff32
unknown <islammohamedahmed49@gmail.com>
2024-03-16 18:40:09 +0200

    horseshoe

---
+++ horseshoe/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+string str = ReadLine();
+var arr1 = str.Split(" ");
+int[] arr2 = new int[4];
+int mx = 0;
+for(int i=0;i<4;i++)
+{
+    arr2[i] = ToInt32(arr1[i]);
+}
+for(int i = 0; i < 4; i++)
+{
+    int sum = 0;
+    for(int j = i + 1; j < 4; j++)
+    {
+        if (arr2[j] == arr2[i])
+        {
+            sum++;
+        }
+    }
+    if (sum >= 2)
+    {
+        mx = sum;
+        break;
+    }
+    else
+    {
+        mx += sum;
+    }
+}
+WriteLine(mx);
